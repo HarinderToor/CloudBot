65d1219e0fa2ac7740989f0f888cff153778860c
Harinder Toor <harindertoor87@gmail.com>
2018-04-25 15:20:52 +0000

    Offtopic - don't ping user

--- plugins/offtopic.py
+++ plugins/offtopic.py
@@ -2,11 +2,11 @@ from cloudbot import hook
 
 
 @hook.command('offtopic')
-def offtopic(text, nick):
+def offtopic(text, nick, message):
     if not text:
         return
     if text.startswith('#'):
         hash_text = 'reddit-tennis-' + text
     else:
         hash_text = '#reddit-tennis-' + text
-    return('Please keep off-topic chat to a minimum, or take it to ' + hash_text)
+    message('Please keep off-topic chat to a minimum, or take it to ' + hash_text)
